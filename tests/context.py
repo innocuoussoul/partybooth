2ddeb3ceb01e89e66c9a45e431625896ce4d5083
Bernd Faßbender <serrin@posteo.de>
2016-12-03 00:36:43 +0100

    Added first Test

--- tests/context.py
+++ tests/context.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 
-import os
 import sys
-
+import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from PartyBooth import PartyBooth
+from lib.PartyBoothController import PartyBoothController
+from lib.pages.ConnectionPage import ConnectionPage
+from lib.pages.CountDownPage import CountDownPage
+from lib.pages.ErrorPage import ErrorPage
+from lib.pages.PhotoReviewPage import PhotoReviewPage
+from lib.pages.StartPage import StartPage
+from lib.CameraAdapter import CameraAdapter
